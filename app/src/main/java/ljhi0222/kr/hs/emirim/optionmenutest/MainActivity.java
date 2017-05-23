4ca260fcef248c35d052690ee882867abb179965
lej3802 <daylock3802@gmail.com>
2017-05-23 19:56:06 +0900

    #3 Option Menu Event Handling

--- app/src/main/java/ljhi0222/kr/hs/emirim/optionmenutest/MainActivity.java
+++ app/src/main/java/ljhi0222/kr/hs/emirim/optionmenutest/MainActivity.java
@@ -1,16 +1,23 @@
 package ljhi0222.kr.hs.emirim.optionmenutest;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    LinearLayout linear;
+    Button but;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        linear = (LinearLayout)findViewById(R.id.linear);
+        but=(Button)findViewById(R.id.but_ok);
     }
 
     @Override
@@ -19,5 +26,28 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater mInflater=getMenuInflater();
         mInflater.inflate(R.menu.menu1, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()){
+            case R.id.item_blue:
+                linear.setBackgroundColor(Color.BLUE);
+                break;
+            case R.id.item_red:
+                linear.setBackgroundColor(Color.RED);
+                break;
+            case R.id.item_yellow:
+                linear.setBackgroundColor(Color.YELLOW);
+                break;
+            case R.id.item_but_rotate:
+                but.setRotation(but.getScaleX()+45);
+                break;
+            case R.id.item_but_size:
+                but.setScaleX(2);
+                break;
+        }
+        return false;
     }
 }
